3905b772e0e9a31499189d3fe2c672ea8a50a5df
eslammasry1 <eslam.mostafa.omer@gmail.com>
2026-09-14 06:54:25 +0300

    Add files via upload

---
+++ 19 - Full Real Project - DVLD/Drivers/frmDrivers.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace _19___Full_Real_Project___DVLD2
+{
+    partial class frmDrivers
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cbFilter = new System.Windows.Forms.ComboBox();
+            this.tbFilter = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblRecords = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToOrderColumns = true;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.White;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.5F);
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle1;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 369);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(1136, 320);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(14, 336);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 22);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Filter By:";
+            // 
+            // cbFilter
+            // 
+            this.cbFilter.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbFilter.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbFilter.FormattingEnabled = true;
+            this.cbFilter.Items.AddRange(new object[] {
+            "none",
+            "DriverID",
+            "PersonID",
+            "NationalNO",
+            "FullName"});
+            this.cbFilter.Location = new System.Drawing.Point(101, 336);
+            this.cbFilter.Name = "cbFilter";
+            this.cbFilter.Size = new System.Drawing.Size(121, 26);
+            this.cbFilter.TabIndex = 8;
+            this.cbFilter.SelectedIndexChanged += new System.EventHandler(this.cbFilter_SelectedIndexChanged);
+            // 
+            // tbFilter
+            // 
+            this.tbFilter.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbFilter.Location = new System.Drawing.Point(228, 336);
+            this.tbFilter.Name = "tbFilter";
+            this.tbFilter.Size = new System.Drawing.Size(226, 27);
+            this.tbFilter.TabIndex = 7;
+            this.tbFilter.TextChanged += new System.EventHandler(this.tbFilter_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Maroon;
+            this.label2.Location = new System.Drawing.Point(426, 278);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(313, 46);
+            this.label2.TabIndex = 59;
+            this.label2.Text = "Manage Drivers";
+            // 
+            // lblRecords
+            // 
+            this.lblRecords.AutoSize = true;
+            this.lblRecords.Font = new System.Drawing.Font("MS Reference Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRecords.Location = new System.Drawing.Point(145, 695);
+            this.lblRecords.Name = "lblRecords";
+            this.lblRecords.Size = new System.Drawing.Size(40, 26);
+            this.lblRecords.TabIndex = 62;
+            this.lblRecords.Text = "00";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("MS Reference Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(7, 695);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(133, 26);
+            this.label3.TabIndex = 61;
+            this.label3.Text = "# Records:";
+            // 
+            // btnClose
+            // 
+            this.btnClose.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnClose.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClose.Image = global::_19___Full_Real_Project___DVLD2.Properties.Resources.close2;
+            this.btnClose.ImageAlign = System.Drawing.ContentAlignment.BottomLeft;
+            this.btnClose.Location = new System.Drawing.Point(1010, 695);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(138, 48);
+            this.btnClose.TabIndex = 60;
+            this.btnClose.Text = "Close";
+            this.btnClose.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnClose.UseVisualStyleBackColor = false;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::_19___Full_Real_Project___DVLD2.Properties.Resources.Driver_Icon;
+            this.pictureBox1.Location = new System.Drawing.Point(411, 6);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(345, 269);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 58;
+            this.pictureBox1.TabStop = false;
+            // 
+            // frmDrivers
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1160, 751);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblRecords);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cbFilter);
+            this.Controls.Add(this.tbFilter);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "frmDrivers";
+            this.Text = "frmDrivers";
+            this.Load += new System.EventHandler(this.frmDrivers_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cbFilter;
+        private System.Windows.Forms.TextBox tbFilter;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Label lblRecords;
+        private System.Windows.Forms.Label label3;
+    }
+}
---
+++ 19 - Full Real Project - DVLD/Drivers/frmDrivers.cs
@@ -0,0 +1,77 @@
+﻿using DVLD_BusinessLayer;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _19___Full_Real_Project___DVLD2
+{
+    public partial class frmDrivers : Form
+    {
+        public frmDrivers()
+        {
+            InitializeComponent();
+        }
+
+        private void frmDrivers_Load(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = clsDriver.GetDrivers();
+            lblRecords.Text = dataGridView1.RowCount.ToString();
+            cbFilter.SelectedIndex = 0;
+        }
+
+        private void tbFilter_TextChanged(object sender, EventArgs e)
+        {
+            if (tbFilter.Text == "")
+            {
+                dataGridView1.DataSource = clsDriver.GetDrivers();
+                lblRecords.Text = dataGridView1.RowCount.ToString();
+            }
+            if (cbFilter.SelectedIndex == 1 && tbFilter.Text != "")
+            {
+                dataGridView1.DataSource = clsDriver.GetDriversFilterByDriverID(int.Parse(tbFilter.Text));
+                lblRecords.Text = dataGridView1.RowCount.ToString();
+            }
+            if (cbFilter.SelectedIndex == 2 && tbFilter.Text != "")
+            {
+                dataGridView1.DataSource = clsDriver.GetDriversFilterByPersonID(int.Parse(tbFilter.Text));
+                lblRecords.Text = dataGridView1.RowCount.ToString();
+            }
+            if (cbFilter.SelectedIndex == 3 && tbFilter.Text != "")
+            {
+                dataGridView1.DataSource = clsDriver.GetAllDriversFilterByNationalNo(tbFilter.Text);
+                lblRecords.Text = dataGridView1.RowCount.ToString();
+            }
+            if (cbFilter.SelectedIndex == 4 && tbFilter.Text != "")
+            {
+                dataGridView1.DataSource = clsDriver.GetAllUsersFilterByFullName(tbFilter.Text);
+                lblRecords.Text = dataGridView1.RowCount.ToString();
+            }
+
+        }
+
+        private void cbFilter_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cbFilter.SelectedIndex == 0)
+            {
+                dataGridView1.DataSource = clsDriver.GetDrivers();
+                lblRecords.Text = dataGridView1.RowCount.ToString();
+                tbFilter.Visible = false;
+            }
+            else
+            {
+                tbFilter.Visible = true;
+            }
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
